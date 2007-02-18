0e1a33bc5fd8d78f6abdf9d0b344130c17bf764e
zaurum <zaurum>
2007-02-18 05:51:19 +0000

    Stub library file and license information added

---
+++ svlite/svlite.lib.php
@@ -0,0 +1,31 @@
+<?php
+//
+// $Id: svlite.lib.php,v 1.1 2007/02/18 05:51:19 zaurum Exp $
+// Author: Konstantin Boyandin <konstantin@boyandin.ru>
+//
+// This file is a part of SVLite MVC framework distribution
+// http://siteverse.com
+//
+// svlit.lib.php: main SVLite library file (PHP 4.3.0+ compatible)
+// Copyright (c) 2005,2006,2007 Konstantin Boyandin. All rights reserved.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+//
+// $Log: svlite.lib.php,v $
+// Revision 1.1  2007/02/18 05:51:19  zaurum
+// Stub library file and license information added
+//
+//
+?>
